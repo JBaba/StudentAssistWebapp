14769b72769c109e59ed5c5b7c82779a1c93c98a
apurv2 <apurv2@gmail.com>
2016-04-26 20:49:54 -0400

    Commit from JBoss Tools

---
+++ src/main/java/com/apurv/studentssist/ApplicationApi.java
@@ -0,0 +1,13 @@
+package com.apurv.studentssist;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+@ApplicationPath("rest")
+public class ApplicationApi extends Application{
+
+	
+	
+	
+}
